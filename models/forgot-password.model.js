de3e184a98dfc244671e1d7713f814e7bb3d3918
namhoangg <nam.hoangg142@gmail.com>
2024-05-02 22:38:29 +0700

    first commit

--- models/forgot-password.model.js
+++ models/forgot-password.model.js
@@ -1,16 +1,20 @@
 const mongoose = require("mongoose");
 const forgotPasswordSchema = new mongoose.Schema(
   {
-    email:String,
-    otp:String,
-    expireAt:{
-      type:Date,
-      expires:180,
+    email: String,
+    otp: String,
+    expireAt: {
+      type: Date,
+      expires: '3m',
     },
   },
   {
     timestamps: true,
   }
 );
-const ForgotPassword= mongoose.model("ForgotPassword", forgotPasswordSchema, "forgot-password");
+const ForgotPassword = mongoose.model(
+  "ForgotPassword",
+  forgotPasswordSchema,
+  "forgot-password"
+);
 module.exports = ForgotPassword;
